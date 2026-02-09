cd2d405e713e5684b2dc9943aa33beb806dd2202
Isabella751 <isabella.leite562@gmail.com>
2026-02-09 11:07:45 -0300

    Fazendo o cadastrar turma

--- Atividade_2_Java/src/Main.java
+++ Atividade_2_Java/src/Main.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     private static ArrayList <Aluno> listaAlunos = new ArrayList<>();
@@ -22,13 +20,14 @@ public class Main {
         switch (opcao){
             case "1":
                 menuAlunos();
-
+                break;
             case "2":
                 menuTurmas();
-
+                break;
             case "3":
                 System.out.println("Até breve...");
                 System.exit(0);
+                break;
             default:
                 System.out.println("Opção inválida! Tente novamente.");
                 menu();
@@ -47,6 +46,7 @@ public class Main {
         switch (opcao){
             case "1":
                 listarTurmas();
+                menuTurmas();
                 break;
             case "2":
                 cadastrarTurma();
@@ -78,6 +78,7 @@ public class Main {
         switch (opcao){
             case "1":
                 listarAlunos();
+                menuAlunos();
                 break;
             case "2":
                 cadastrarAluno();
@@ -104,11 +105,70 @@ public class Main {
     }
 
     private static void cadastrarTurma() {
+        Periodo periodo = validarPeriodo();
+
+        String curso = Leitura.dados("Digite o curso:  ");
+        while(!isCharacter(curso)) {
+            System.out.println("Nome de curso inválido! Não use números ou caracteres especiais, por favor.");
+            curso = Leitura.dados("Digite o curso:  ");
+        }
+
+        String sigla = Leitura.dados("Digite a sigla:  ");
+        boolean repetido = true;
+        while(sigla.isBlank() && !repetido) {
+            System.out.println("Sigla inválida!");
+            sigla = Leitura.dados("Digite a sigla:  ");
+            sigla = sigla.toUpperCase();
+        }
+
+        for(Turma t: listaTurmas) {
+            if (t.getSigla().equals(sigla)) {
+                System.out.println("Turma já cadastrada.");
+                repetido = true;
+            }
+        }
+
+        repetido = false;
+
+        Turma turma = new Turma(curso, sigla, periodo);
+        listaTurmas.add(turma);
+        }
+
+    private static boolean isCharacter(String curso) {
+            String cursoSemNumeros = curso.replaceAll("\\d", "");
+            return !curso.isBlank() && curso.equals(cursoSemNumeros);
+        }
+
+
+    private static Periodo validarPeriodo() {
+        String opcaoPeriodo = Leitura.dados("""
+                Escolha o período:
+                1 - Matutino
+                2 - Vespertino
+                3 - Noturno
+                4 - Integral""");
+        switch (opcaoPeriodo){
+            case "1":
+                return Periodo.MATUTINO;
+            case "2":
+                return Periodo.VESPERTINO;
+            case "3":
+                return Periodo.NOTURNO;
+            case "4":
+                return Periodo.INTEGRAL;
+            default:
+                System.out.println("Opção inválida. Digite novamente!");
+                return validarPeriodo();
+        }
     }
 
     private static void listarTurmas() {
-        for(Turma b: listaTurmas){
-            System.out.println(b);
+        if(listaTurmas.isEmpty()) {
+            System.out.println("Não há turmas cadastradas.");
+            return;
+        }
+        for(Turma t: listaTurmas){
+            System.out.println(t);
         }
     }
 
@@ -122,6 +182,10 @@ public class Main {
     }
 
     private static void listarAlunos() {
+        if(listaAlunos.isEmpty()){
+            System.out.println("Não há alunos cadastrados.");
+            return;
+        }
         for(Aluno a: listaAlunos){
             System.out.println(a);
         }
--- Atividade_2_Java/src/Turma.java
+++ Atividade_2_Java/src/Turma.java
@@ -1,5 +1,38 @@
 public class Turma {
     private String curso;
     private String sigla;
+
+    public Turma(String curso, String sigla, Periodo periodo) {
+        this.curso = curso;
+        this.sigla = sigla;
+        this.periodo = periodo;
+    }
+
+
+
     private Periodo periodo;
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Periodo getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Periodo periodo) {
+        this.periodo = periodo;
+    }
 }
