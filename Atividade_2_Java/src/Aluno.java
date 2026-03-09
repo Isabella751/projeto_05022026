f68bcedb14cc51709389bc40b9957578bd25025e
Isabella751 <isabella.leite562@gmail.com>
2026-03-09 11:17:53 -0300

    Fazendo o listar alunos

--- Atividade_2_Java/src/Aluno.java
+++ Atividade_2_Java/src/Aluno.java
@@ -2,8 +2,42 @@ import java.time.LocalDate;
 
 public class Aluno {
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public boolean isAtivoAluno() {
+        return Ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        Ativo = ativo;
+    }
+
     private LocalDate dataNascimento;
     private Turma turma;
+    private boolean Ativo;
 }
 
 
